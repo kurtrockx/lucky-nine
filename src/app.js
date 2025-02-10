ef82b55a8a789924efd88e7bcccac645d01460e0
derpfear <kurtdebelen431@gmail.com>
2025-02-10 23:52:33 +0800

    added face cards toggle

--- src/app.js
+++ src/app.js
@@ -1,17 +1,6 @@
 import "./index.css";
 import { useEffect, useRef, useState } from "react";
 
-const drawCards = async (count) => {
-  const res = await fetch(
-    `https://deckofcardsapi.com/api/deck/new/draw/?count=${count}`
-  );
-  const data = await res.json();
-  const cards = data.cards;
-  console.log(cards);
-  return cards;
-};
-
-drawCards(52);
 export default function App() {
   const [gameStarted, setGameStarted] = useState(false);
   const [score, setScore] = useState(0);
@@ -20,6 +9,74 @@ export default function App() {
   const [playerLost, setPlayerLost] = useState(false);
   const [playerCards, setPlayerCards] = useState([0, 0, 0]);
   const [oppCards, setOppCards] = useState([0, 0, 0]);
+  const [toggledFaceCards, setToggledFaceCards] = useState(true);
+
+  const drawCards = async (count) => {
+    if (toggledFaceCards === true) {
+      const res = await fetch(
+        `https://deckofcardsapi.com/api/deck/new/draw/?count=${count}`
+      );
+
+      const data = await res.json();
+      const cards = data.cards;
+      return cards;
+    }
+
+    const customCards = [
+      "AS",
+      "AH",
+      "AD",
+      "AC",
+      "2S",
+      "3S",
+      "4S",
+      "5S",
+      "6S",
+      "7S",
+      "8S",
+      "9S",
+      "10S",
+      "2H",
+      "3H",
+      "4H",
+      "5H",
+      "6H",
+      "7H",
+      "8H",
+      "9H",
+      "10H",
+      "2D",
+      "3D",
+      "4D",
+      "5D",
+      "6D",
+      "7D",
+      "8D",
+      "9D",
+      "10D",
+      "2C",
+      "3C",
+      "4C",
+      "5C",
+      "6C",
+      "7C",
+      "8C",
+      "9C",
+      "10C",
+    ].join(",");
+
+    const deckUrl = `https://deckofcardsapi.com/api/deck/new/shuffle/?cards=${customCards}`;
+    const deckResponse = await fetch(deckUrl);
+    const deckData = await deckResponse.json();
+    const deckId = deckData.deck_id;
+
+    const res = await fetch(
+      `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=${count}`
+    );
+    const data = await res.json();
+    const cards = data.cards;
+    return cards;
+  };
 
   return (
     <div className="app">
@@ -31,6 +88,7 @@ export default function App() {
         score={score}
       />
       <Player
+        drawCards={drawCards}
         setGameStarted={setGameStarted}
         gameStarted={gameStarted}
         setScore={setScore}
@@ -46,6 +104,28 @@ export default function App() {
         oppValue={oppValue}
       />
       <Score score={score} gameStarted={gameStarted} />
+      <FaceCardsButton
+        setToggledFaceCards={setToggledFaceCards}
+        toggledFaceCards={toggledFaceCards}
+        drawCards={drawCards}
+      />
+    </div>
+  );
+}
+
+function FaceCardsButton({ setToggledFaceCards, toggledFaceCards, drawCards }) {
+  return (
+    <div
+      onClick={() => setToggledFaceCards((f) => !f)}
+      className={`face-card-button ${
+        toggledFaceCards ? "face-cards-on" : "face-cards-off"
+      }`}
+    >
+      <div
+        className={`toggle-element ${
+          toggledFaceCards ? "toggled-on" : "toggled-off"
+        }`}
+      ></div>
     </div>
   );
 }
@@ -67,6 +147,7 @@ function Opponent({ gameStarted, oppCards, oppValue, score, playerLost }) {
 }
 
 function Player({
+  drawCards,
   setGameStarted,
   gameStarted,
   setPlayerLost,
